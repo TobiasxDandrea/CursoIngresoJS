4855a2d3d181e88af32108a49ff8fe53d438429c
TobiasxDandrea <tdandrea01@gmail.com>
2021-08-05 16:16:43 -0300

    tercer commit

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,17 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Esto funciona de maravilla!! :)");
+
+	let nombre;
+	nombre = prompt ("Ingrese nombre");
+	alert (nombre);
+
+
+	/* let nombre; declaro la variable
+	nombre = prompt ("Ingrese su nombre"); hago prompt para que el usuario ingrese el dato
+	alert (nombre); muestro por alert el dato que ingresa el usuario
+	prompt y alert nunca con= porque no estamos declarando ninguna variable, solamente
+	son carteles de ingreso de datos y mostrar mensaje
+	prompt devuelve lo escrito en su caja de texto */
 }
 
